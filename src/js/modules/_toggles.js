10316557713c7cd3520e8ea8de9b79765df4d69a
Valeria K <laaurora.val@gmail.com>
2017-02-21 11:21:32 +0300

    Add hide on click outside

--- src/js/modules/_toggles.js
+++ src/js/modules/_toggles.js
@@ -1,24 +1,39 @@
 (function ($) {
 
-  var $togglePathologiesBtn = $('.js-dropdown-btn');
-  var $toggledPathologies = $('.js-dropdown');
+  var $toggleMenuBtn = $('.js-dropdown-btn');
+  var $toggledMenu = $('.js-dropdown');
   var $toggleSearchBtn = $('.js-search-btn');
   var $toggledSearch = $('.js-search');
 
-  function togglePathologies (e) {
-    $togglePathologiesBtn.toggleClass('is-open');
-    $toggledPathologies.toggleClass('is-open');
+  function toggleMenu (e) {
+    $toggleMenuBtn.toggleClass('is-open');
+    $toggledMenu.toggleClass('is-open');
   }
 
-    function toggleSearch (e) {
+  function toggleSearch (e) {
     $toggleSearchBtn.toggleClass('is-open');
     $toggledSearch.toggleClass('is-open');
   }
 
+  function hideMenu (e) {
+    if (!$(e.target).closest($toggledMenu).length && !$(e.target).closest($toggleMenuBtn).length) {
+    $toggleMenuBtn.removeClass('is-open');
+    $toggledMenu.removeClass('is-open');
+    }
+  }
+
+    function hideSearch (e) {
+      if (!$(e.target).closest($toggledSearch).length && !$(e.target).closest($toggleSearchBtn).length) {
+      $toggleSearchBtn.removeClass('is-open');
+      $toggledSearch.removeClass('is-open');
+      }
+    }
 
   $(document).ready(function () {
-    $togglePathologiesBtn.on('click', togglePathologies);
+    $toggleMenuBtn.on('click', toggleMenu);
     $toggleSearchBtn.on('click', toggleSearch);
+    $('html').on('click', hideMenu);
+    $('html').on('click', hideSearch);
   });
 
 })(jQuery);
