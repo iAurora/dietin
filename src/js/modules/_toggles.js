8082da8a69cf6aea63e04fc12862aa9ce8265f5d
Valeria K <laaurora.val@gmail.com>
2017-02-24 18:17:56 +0300

    Adjust toggles JS

--- src/js/modules/_toggles.js
+++ src/js/modules/_toggles.js
@@ -1,64 +1,65 @@
 (function ($) {
 
-  var $html = $('html');
-  var $toggleMenuBtn = $('.js-dropdown-btn');
-  var $toggledMenu = $('.js-dropdown');
-  var $toggleSearchBtn = $('.js-search-btn');
-  var $toggledSearch = $('.js-search');
-  var $toggleMobileBtn = $('.js-mobile-nav-btn');
-  var $toggledMobile = $('.js-mobile-nav');
-  var $mobileOverlay = $('.js-mobile-nav-overlay');
+  $(function (){
 
-  function toggleMenu (e) {
-    $toggleMenuBtn.toggleClass('is-open');
-    $toggledMenu.toggleClass('is-open');
-  }
+    var $html = $('html');
+    var $toggleMenuBtn = $('.js-dropdown-btn');
+    var $toggledMenu = $('.js-dropdown');
+    var $toggleSearchBtn = $('.js-search-btn');
+    var $toggledSearch = $('.js-search');
+    var $toggleMobileBtn = $('.js-mobile-nav-btn');
+    var $toggledMobile = $('.js-mobile-nav');
+    var $mobileOverlay = $('.js-mobile-nav-overlay');
 
-  function toggleSearch (e) {
-    $toggleSearchBtn.toggleClass('is-open');
-    $toggledSearch.toggleClass('is-open');
-  }
-
-   function toggleMobile (e) {
-    $toggleMobileBtn.toggleClass('is-open');
-    $toggledMobile.toggleClass('is-open');
-    $mobileOverlay.toggle();
-  }
-
-  function hideMenu (e) {
-    if (!$(e.target).closest($toggledMenu).length && !$(e.target).closest($toggleMenuBtn).length) {
-    $toggleMenuBtn.removeClass('is-open');
-    $toggledMenu.removeClass('is-open');
-    }
-  }
-
-    function hideSearch (e) {
-      if (!$(e.target).closest($toggledSearch).length && !$(e.target).closest($toggleSearchBtn).length) {
-      $toggleSearchBtn.removeClass('is-open');
-      $toggledSearch.removeClass('is-open');
-      }
+    function toggleMenu (e) {
+      $toggleMenuBtn.toggleClass('is-open');
+      $toggledMenu.toggleClass('is-open');
     }
 
-    function hideMobile (e) {
-      if (!$(e.target).closest($toggledMobile).length && !$(e.target).closest($toggleMobileBtn).length) {
-      $toggleMobileBtn.removeClass('is-open');
-      $toggledMobile.removeClass('is-open');
-      }
+    function toggleSearch (e) {
+      $toggleSearchBtn.toggleClass('is-open');
+      $toggledSearch.toggleClass('is-open');
     }
 
-  $(document).ready(function () {
-    $toggleMenuBtn.on('click', toggleMenu);
-    $toggleSearchBtn.on('click', toggleSearch);
-    $toggleMobileBtn.on('click', toggleMobile);
-    $html.on('click', hideMenu);
-    $html.on('click', hideSearch);
-    $html.on('click', hideMobile);
-
-    $mobileOverlay.click(function(){
-      $mobileOverlay.toggle();
+    function toggleMobile (e) {
       $toggleMobileBtn.toggleClass('is-open');
       $toggledMobile.toggleClass('is-open');
-    });
+      $mobileOverlay.toggle();
+    }
+
+    function hideMenu (e) {
+      if (!$(e.target).closest($toggledMenu).length && !$(e.target).closest($toggleMenuBtn).length) {
+      $toggleMenuBtn.removeClass('is-open');
+      $toggledMenu.removeClass('is-open');
+      }
+    }
+
+      function hideSearch (e) {
+        if (!$(e.target).closest($toggledSearch).length && !$(e.target).closest($toggleSearchBtn).length) {
+        $toggleSearchBtn.removeClass('is-open');
+        $toggledSearch.removeClass('is-open');
+        }
+      }
+
+      function hideMobile (e) {
+        if (!$(e.target).closest($toggledMobile).length && !$(e.target).closest($toggleMobileBtn).length) {
+        $toggleMobileBtn.removeClass('is-open');
+        $toggledMobile.removeClass('is-open');
+        }
+      }
+
+      $toggleMenuBtn.on('click', toggleMenu);
+      $toggleSearchBtn.on('click', toggleSearch);
+      $toggleMobileBtn.on('click', toggleMobile);
+      $html.on('click', hideMenu);
+      $html.on('click', hideSearch);
+      $html.on('click', hideMobile);
+
+      $mobileOverlay.click(function(){
+        $mobileOverlay.toggle();
+        $toggleMobileBtn.toggleClass('is-open');
+        $toggledMobile.toggleClass('is-open');
+      });
 
   });
 
